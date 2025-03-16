d7d71af19a90a2a885962a665c6d1d2bab8d73e6
sohaib hassan <sohaibhassancc@gmail.com>
2025-03-16 20:54:19 +0500

    changes from lcoal repo

--- learn_39.php
+++ learn_39.php
@@ -1,3 +1,13 @@
-<<<<<<< HEAD
-<?php echo "Updated from Gitbash"; ?>
->>>>>>> feature
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP File</title>
+</head>
+<body>
+    <?php echo "<h1>Sohaib Hassan</h1>"; ?>
+   <?php echo "<p>Changes from Remote Repository </p>" ?>
+   <!-- New Changes from local Repo -->
+</body>
+</html>
