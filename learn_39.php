c84e493d2d616b1673e8425c74e4253b101977b5
sohaib hassan <sohaibhassancc@gmail.com>
2025-03-16 20:24:29 +0500

    Initial Commit add Echo Feature with Roll_No

---
+++ learn_39.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP File</title>
+</head>
+<body>
+    <?php echo "<h1>Sohaib Hassan</h1>"; ?>
+</body>
+</html>
