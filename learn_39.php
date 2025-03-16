4cde31af0c1003f29ef0975fa865b83556cfd799
sohaib hassan <sohaibhassancc@gmail.com>
2025-03-16 20:43:40 +0500

    Updated from Gitbash

--- learn_39.php
+++ learn_39.php
@@ -1,11 +1,1 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP File</title>
-</head>
-<body>
-    <?php echo "<h1>Sohaib Hassan</h1>"; ?>
-</body>
-</html>
+<?php "</h1> Updated from gitbash</h1>"; ?>
