4f7a686823b327c1eadc45f4c035c417f589c61b
sohaib hassan <sohaibhassancc@gmail.com>
2025-03-16 20:40:33 +0500

    updated using gitbash

--- learn_39.php
+++ learn_39.php
@@ -1,12 +1,1 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP File</title>
-</head>
-<body>
-    <?php echo "<h1>Sohaib Hassan</h1>"; ?>
-   <?php echo "<p>Changes from Remote Repository </p>" ?>
-</body>
-</html>
+<?php echo "Updated from Gitbash"; ?>
